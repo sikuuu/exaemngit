6f0e2fba6087df96c58891d26fd77dae6c5fb121
Dani Prados <daniprados@gmail.com>
2020-10-29 10:23:51 +0100

    Canvis del programador 1

---
+++ funcions.php
@@ -0,0 +1,15 @@
+<?php
+
+function programador1($n) {
+    $result = 1;
+    $multiplicador = -1;
+    $signe = -1;
+    for ($i=2;$i<$n;$i++) {
+        //echo "$result = $result + $signe / (2 * $i -1);<br>";
+        $result = $result + $signe / (2 * $i -1);
+        $signe = $multiplicador * $signe;
+    }
+    $pi = 4 * $result;
+
+    return $pi;
+}
